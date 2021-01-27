53bb4f183430e7e396bb78ab5e5654c8dfca4eff
PROPHESSOR <xenteze@gmail.com>
2021-01-28 01:50:50 +0200

    Попытка исправить маппинги и рефактор

--- extract.sh
+++ extract.sh
@@ -5,6 +5,3 @@ mv export/*.raw raw/
 find raw/ -name \*.raw -exec convert -size 64x64 -depth 8 rgb:{} -rotate 90 -flop {}.png \;
 mkdir png
 mv raw/*.png png/
-mkdir tiles
-find png/ -name \*.png -exec convert {} -crop 64x64 {}_%d.png \;
-mv png/texels*.png tiles
--- main.js
+++ main.js
@@ -1,12 +1,21 @@
-const PALLETES = '../palettes.bin';
-const WTEXELS = '../wtexels.bin';
+// const PALLETES = '../palettes.bin';
+// const WTEXELS = '../wtexels.bin';
+// const MAPPINGS = '../mappings.bin';
+const PALLETES = 'palettes.bin';
+const WTEXELS = 'wtexels.bin';
+const MAPPINGS = 'mappings.bin';
+
+const PALETTE_SIZE = 16;
+const TEXTURE_SIZE = [64, 64];
+
+const PALETTE_SIZE_0 = PALETTE_SIZE - 1;
 
 function main(args) {
   const a = palette(PALLETES);
 
   console.log(a);
 
-  const m = mappings('../mappings.bin');
+  const m = mappings(MAPPINGS);
 
   console.log(m);
 
@@ -16,15 +25,15 @@ function main(args) {
 }
 
 const fs = require('fs');
-const exec = require("child_process").execSync;
-
 
 function palette(file) {
     var buffer = fs.readFileSync(file);
     var numColors = buffer.readInt32LE(0);
-    paletteColors = [];
+    const paletteColors = [];
 
-    for (var i = 4; i < numColors + 4; i += 2) {
+    for (var i = 4; i < numColors + 4;) {
+      const palette = [];
+      for (var j = 0; j < PALETTE_SIZE; i += 2, j++) {
         var tmp = buffer.readUInt16LE(i);
         var r = tmp & 0x1F;
         r = r << 3 | (r >> 2);
@@ -33,63 +42,38 @@ function palette(file) {
         var b = tmp >> 11 & 0x1F;
         b = b << 3 | (b >> 2);
         var color = (0xFF000000 | r << 16 | g << 8 | b << 0);
-        paletteColors.push(color);
+        palette.push(color);
+      }
+
+      paletteColors.push(palette)
     }
     return paletteColors;
 }
 
-
-
-// function walls(file, paletteColors, mappings) {
-//     var texels = fs.readFileSync(file);
-//     var textureBuffer = texels.slice(4);
-//     var outputBuffer = [];
-//     let i = 0;
-
-//     for (var i = 0; i < paletteColors.length; i += 16) {
-//         for (var j = 0; j < textureBuffer.length / 2048; j++) {
-//           console.log(j, paletteColors[mappings[j]]);
-//             var color1 = paletteColors[mappings[j]]; //(textureBuffer[j] & 0xF) + i];
-//             var red1 = 0xFF & (color1 >> 16);
-//             var blue1 = 0xFF & (color1);
-//             var green1 = 0xFF & (color1 >> 8);
-//             var color2 = paletteColors[mappings[j]];//(textureBuffer[j] >> 4 & 0xF) + i];
-//             var red2 = 0xFF & (color1 >> 16);
-//             var blue2 = 0xFF & (color1);
-//             var green2 = 0xFF & (color1 >> 8);
-
-//             outputBuffer.push(red1, green1, blue1, red2, green2, blue2);
-//         }
-//         fs.writeFileSync("export/texels" + i + ".raw", Buffer.from(outputBuffer));
-//         i++;
-//         outputBuffer = [];
-//     }
-//     return;
-// }
-
-function walls2(file, paletteColors, mappings) {
+function walls2(file, palettes, mappings) {
   var texels = fs.readFileSync(file);
   var textureBuffer = texels.slice(4);
   var outputBuffer = [];
   let i = 0;
 
-  for (const texoffset_ in mappings) {
-    const texoffset = Number(texoffset_);
-    for (var j = texoffset; j < texoffset + 4096; j++) {
-      const palid = mappings[texoffset];
-      // console.log(j, texoffset, palid, paletteColors[(textureBuffer[j] & 0xF) + palid]);
-        var color1 = paletteColors[(textureBuffer[j] & 0xF) + palid]; //(textureBuffer[j] & 0xF) + i];
-        var red1 = 0xFF & (color1 >> 16);
-        var blue1 = 0xFF & (color1);
-        var green1 = 0xFF & (color1 >> 8);
-        var color2 = paletteColors[(textureBuffer[j] >> 4 & 0xF) + palid];//(textureBuffer[j] >> 4 & 0xF) + i];
-        var red2 = 0xFF & (color1 >> 16);
-        var blue2 = 0xFF & (color1);
-        var green2 = 0xFF & (color1 >> 8);
+  const textureSize = TEXTURE_SIZE[0] * TEXTURE_SIZE[1];
 
-        outputBuffer.push(red1, green1, blue1, red2, green2, blue2);
+  for (const [texoffset, palid] of mappings) {
+
+    for (var j = texoffset; j < texoffset + textureSize; j++) {
+      const pixel = textureBuffer[j];
+      var color1 = palettes[palid][pixel & 0xF]; //(textureBuffer[j] & 0xF) + i];
+      var red1 = 0xFF & (color1 >> 16);
+      var green1 = 0xFF & (color1 >> 8);
+      var blue1 = 0xFF & (color1);
+      var color2 = palettes[palid][pixel >> 4 & 0xF];//(textureBuffer[j] >> 4 & 0xF) + i];
+      var red2 = 0xFF & (color2 >> 16);
+      var green2 = 0xFF & (color2 >> 8);
+      var blue2 = 0xFF & (color2);
+
+      outputBuffer.push(red1, green1, blue1, red2, green2, blue2);
     }
-    fs.writeFileSync("export/texels" + i + ".raw", Buffer.from(outputBuffer));
+    fs.writeFileSync(`export/texel_${texoffset / textureSize}_palette_${palid}.raw`, Buffer.from(outputBuffer));
     i++;
     outputBuffer = [];
   }
@@ -98,23 +82,22 @@ function walls2(file, paletteColors, mappings) {
 function mappings(file) {
   var buffer = fs.readFileSync(file);
   let offset = 0;
-  var count1 = buffer.readInt32LE(offset);
+  var texturesCount = buffer.readInt32LE(offset);
+  console.log('found ' + texturesCount + ' mappings');
   offset += 4;
-  var count2 = buffer.readInt32LE(offset);
+  var spritesCount = buffer.readInt32LE(offset);
   offset += 4;
-  var count3 = buffer.readInt32LE(offset);
-  offset += 4;
-  var count4 = buffer.readInt32LE(offset);
-  offset += 4;
+  offset += 4; // unused
+  offset += 4; // unused
 
-  const dict = {};
+  const dict = [];
 
-  for (let i = 0; i < count1; i++) {
-    const texelid = buffer.readUInt32LE(offset);// / 4096;
+  for (let i = 0; i < texturesCount; i++) {
+    const texeloffset = buffer.readUInt32LE(offset);// / 4096;
     offset += 4;
     const palid = buffer.readUInt32LE(offset) / 16;
     offset += 4;
-    dict[texelid] = palid;
+    dict.push([texeloffset, palid]);
   }
   return dict;
 }
